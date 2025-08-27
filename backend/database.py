1afae0144373ada6f0c6dad09d0d7de3117aba9b
Gavriloxana <86794272+Gavriloxana@users.noreply.github.com>
2025-08-27 11:14:27 +0700

    END_miniPJ

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,21 @@
+import os
 from mongoengine import connect
+from dotenv import load_dotenv
 
-# เชื่อม MongoDB Local
-connect(db="pos_db", host="localhost", port=27017)
+# โหลดค่า .env
+load_dotenv()
 
-# หรือ MongoDB Atlas
-# connect(db="pos_db", host="mongodb+srv://<username>:<password>@cluster0.mongodb.net/pos_db?retryWrites=true&w=majority")
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_PORT = os.getenv("MONGO_PORT")
+MONGO_DB   = os.getenv("MONGO_DB")
+MONGO_AUTH = os.getenv("MONGO_AUTH_DB")
+
+# สร้าง URI (เข้ารหัสรหัสผ่านอัตโนมัติถ้ามีอักขระพิเศษ)
+from urllib.parse import quote_plus
+MONGO_PASS_ENC = quote_plus(MONGO_PASS)
+
+uri = f"mongodb://{MONGO_USER}:{MONGO_PASS_ENC}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource={MONGO_AUTH}"
+
+connect(db=MONGO_DB, host=uri)
--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,10 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8100",
+    "http://localhost:8000",
+    "http://localhost:8100",
 ]
 
 app.add_middleware(
