a2f297624773bdef23766a8bd50c2e8118dda5e5
Gavriloxana <86794272+Gavriloxana@users.noreply.github.com>
2025-08-27 15:46:35 +0700

    bro again

--- backend/main.py
+++ backend/main.py
@@ -6,30 +6,14 @@ from users import router as users_router
 from products import router as products_router
 from pos import router as pos_router
 from reports import router as reports_router
-import database  # ensure Mongo connectionnnn
+import database  # ensure Mongo connection
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8000",
-    "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:8100",
-    "http://localhost:8000",
-    "http://localhost:8100",
-    "http://192.168.10.118:8000",
-    "http://192.168.10.118:8100",
-    "https://api-bakery.loeitech.org",
-    "https://bakery.loeitech.org"
-    
-
-]
-
+# ✅ Allow all hosts
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -40,6 +24,7 @@ static_images_dir = os.path.join(os.path.dirname(__file__), "images")
 os.makedirs(static_images_dir, exist_ok=True)
 app.mount("/static/images", StaticFiles(directory=static_images_dir), name="images")
 
+# Routers
 app.include_router(users_router, prefix="/users", tags=["Users"])
 app.include_router(products_router, prefix="/products", tags=["Products"])
 app.include_router(pos_router, prefix="/pos", tags=["POS"])
