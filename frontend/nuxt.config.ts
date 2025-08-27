b94dc9b75f8a0a44de83d1778269a72214d6c9a4
Gavriloxana <86794272+Gavriloxana@users.noreply.github.com>
2025-08-27 16:50:40 +0700

    EZ

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,8 +6,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      apiBase: (process.env.NUXT_PUBLIC_API_BASE || 'https://api-bakery.loeitech.org')
-        .replace(/^http:\/\//i, 'https://')
+      apiBase: 'https://api-bakery.loeitech.org'
     }
   },
   vite: {
