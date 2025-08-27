44888ad0312a315e56c393d50ef4baf85ad9bf6c
Gavriloxana <86794272+Gavriloxana@users.noreply.github.com>
2025-08-27 16:32:14 +0700

    Ensure API base URL uses HTTPS by replacing HTTP in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,8 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api-bakery.loeitech.org'
+      apiBase: (process.env.NUXT_PUBLIC_API_BASE || 'https://api-bakery.loeitech.org')
+        .replace(/^http:\/\//i, 'https://')
     }
   },
   vite: {
