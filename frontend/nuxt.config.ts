57ce40dc3ae30db662007e939733b434623e484c
Gavriloxana <86794272+Gavriloxana@users.noreply.github.com>
2025-08-27 17:17:32 +0700

    Update API base URL in nuxt.config.ts to point to the new lab environment

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      apiBase: 'https://api-bakery.loeitech.org'
+      apiBase: 'https://lab.loeitech.org'
     }
   },
   vite: {
